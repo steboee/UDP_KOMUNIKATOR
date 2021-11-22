a6ea082289d13f9036a76abe78135b3335f55dad
Zajo <ckkoval33@gmail.com>
2021-11-22 20:46:06 +0100

    prevzatá checksum ... included .. analyzing

--- Komunikator.py
+++ Komunikator.py
@@ -10,6 +10,57 @@ def switch():
     pass
 
 
+def xor(a, b):
+    result = []
+
+    for i in range(1, len(b)):
+        if a[i] == b[i]:
+            result.append('0')
+        else:
+            result.append('1')
+
+    return ''.join(result)
+
+
+def mod2div(divident, divisor):
+    pick = len(divisor)
+    tmp = divident[0: pick]
+    while pick < len(divident):
+
+        if tmp[0] == '1':
+
+            tmp = xor(divisor, tmp) + divident[pick]
+
+        else:
+            tmp = xor('0' * pick, tmp) + divident[pick]
+
+        pick += 1
+
+    if tmp[0] == '1':
+        tmp = xor(divisor, tmp)
+    else:
+        tmp = xor('0' * pick, tmp)
+    checkword = tmp
+    return checkword
+
+
+
+def decodeData(data, key):
+    l_key = len(key)
+
+    appended_data = data.decode() + '0' * (l_key - 1)
+    remainder = mod2div(appended_data, key)
+    return remainder
+
+
+
+
+
+
+
+
+
+
 def client(client_socket, server_address):
     while True:
 
